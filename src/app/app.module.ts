f561d01ee175a36e36d0e4624da238399039bada
olcay <olcaymaster@gmail.com>
2022-03-04 02:07:36 +0300

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { FileDropComponent } from './file-drop/file-drop.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, FileDropComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
